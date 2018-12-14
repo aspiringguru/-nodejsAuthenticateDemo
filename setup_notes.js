37ee3a93941c78e1a0706df0f1789718a125dc92
aspiringguru <bmatthewtaylor@gmail.com>
2018-12-14 10:13:01 +1000

    first commit

---
+++ setup_notes.js
@@ -0,0 +1,21 @@
+https://scotch.io/tutorials/easy-node-authentication-setup-and-local
+https://docs.mongodb.com/manual/tutorial/install-mongodb-on-windows/
+add installed dir to path > C:\Program Files\MongoDB\Server\4.0\bin
+on windows to run MongoDB  mongodb.exe
+
+
+mkdir app
+mkdir app/models
+mkdir config
+mkdir views
+touch server.js
+touch app/models/user.js
+touch app/models/routes.js
+touch config/auth.js
+touch config/database.js
+touch config/passport.js
+touch views/index.ejs
+touch views/login.ejs
+touch views/signup.ejs
+touch views/profile.ejs
+touch server.js
